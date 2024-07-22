437a79ffbbf80a49c439ec863c793b6d2982c2f8
Ryan Campbell <bigtallcampbell@gmail.com>
2024-07-22 15:16:28 +0000

    Fixing sequencing typo

--- src/Program.cs
+++ src/Program.cs
@@ -37,7 +37,6 @@ public class Program {
                 await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
             });
         });
-        app.Run();
 
         // Add a middleware to catch exceptions and stop the host gracefully
         app.Use(async (context, next) => {
@@ -51,5 +50,7 @@ public class Program {
                 lifetime?.StopApplication();
             }
         });
+
+        app.Run();
     }
 }
